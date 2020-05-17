975c06c6077e177a969f094a16a63de8e45024a4
Leonardo Amorim <leonardoaraujodf@gmail.com>
2020-05-17 15:40:12 -0300

    Adding some explanation of how to traverse a bitree

--- includes/traverse.h
+++ includes/traverse.h
@@ -14,6 +14,19 @@
 /**
  * @brief Traverse the binary tree using a preorder traversal, inserting the data elements that are
  * find.
+ * 
+ * Consider a tree like that:
+ * 
+ *                          11
+ *                        /    \
+ *                      51     65
+ *                     / \     / \
+ *                   14   32  23  17
+ *                  / \
+ *                74   10
+ * 
+ * Using a preorder traversal yields: 11 51 14 74 10 32 65 23 17
+ * 
  * @param[in] node The node to be verified.
  * @param[in,out] list The list to insert the elements in the order specified.
  * 
@@ -23,6 +36,19 @@ int preorder(const BiTreeNode *node, List *list);
 /**
  * @brief Traverse the binary tree using a inorder traversal, inserting the data elements that are
  * find.
+ * 
+ * Consider a tree like that:
+ * 
+ *                          11
+ *                        /    \
+ *                      51     65
+ *                     / \     / \
+ *                   14   32  23  17
+ *                  / \
+ *                74   10
+ * 
+ * Using a inorder traversal yields: 74 14 10 51 32 11 23 65 17
+ * 
  * @param[in] node The node to be verified.
  * @param[in,out] list The list to insert the elements in the order specified.
  * 
@@ -32,6 +58,18 @@ int inorder(const BiTreeNode *node, List *list);
 /**
  * @brief Traverse the binary tree using a postorder traversal, inserting the data elements that are
  * find.
+ * Consider a tree like that:
+ * 
+ *                          11
+ *                        /    \
+ *                      51     65
+ *                     / \     / \
+ *                   14   32  23  17
+ *                  / \
+ *                74   10
+ * 
+ * Using a postorder traversal yields: 74 10 14 32 51 23 17 65 11
+ * 
  * @param[in] node The node to be verified.
  * @param[in,out] list The list to insert the elements in the order specified.
  * 
